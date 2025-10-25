46c08e4ce4dc38a5ade0599fd0ec12921ad242c1
Graham Flaspoehler <grahamflas@gmail.com>
2025-10-25 16:58:29 +0200

    Implement ANSWERS constant and #ask functionality

--- lib/magic_ball.rb
+++ lib/magic_ball.rb
@@ -1,7 +1,34 @@
 class MagicBall
-  ANSWERS = []
+  ANSWERS = [
+    "It is certain",
+    "It is decidedly so",
+    "Without a doubt",
+    "Yes, definitely",
+    "You may rely on it",
+    "As I see it, yes",
+    "Most likely",
+    "Outlook good",
+    "Yes",
+    "Signs point to yes",
+    "Reply hazy try again",
+    "Ask again later",
+    "Better not tell you now",
+    "Cannot predict now",
+    "Concentrate and ask again",
+    "Don't count on it",
+    "My reply is no",
+    "My sources say no",
+    "Outlook not so good",
+    "Very doubtful",
+  ].freeze
 
- def ask(question)
-  "Whatever"
- end
+  INVALID_FORMAT_ERROR = "Question has invalid format.".freeze
+
+  def ask(question)
+    if question.is_a?(String) && question[-1] == "?"
+      ANSWERS.sample
+    else
+      raise INVALID_FORMAT_ERROR
+    end
+  end
 end
